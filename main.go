67804d4baea9a12a9b03646efe91f7a8a39bff95
Albert Skonieczny <albertskonieczny@gmail.com>
2024-02-16 11:35:11 +0100

    init

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"time"
+
+// 	"github.com/progrium/macdriver/cocoa"
+// 	"github.com/progrium/macdriver/dispatch"
+// 	"github.com/progrium/macdriver/macos"
+// 	"github.com/progrium/macdriver/objc"
+// )
+
