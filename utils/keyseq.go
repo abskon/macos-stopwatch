e7e1539527b7bd1fd70abfd8db6b4b0d06ddbbac
Albert Skonieczny <albertskonieczny@gmail.com>
2024-02-17 18:14:23 +0100

    KeySeq.Str() refactor

--- utils/keyseq.go
+++ utils/keyseq.go
@@ -28,7 +28,7 @@ func NewKeySeq() *KeySeq {
 	}
 }
 
-func (ks *KeySeq) IsPressed(key any) bool {
+func (ks *KeySeq) isPressed(key any) bool {
 	switch k := key.(type) {
 	case uint64:
 		pressed, exists := ks.seq.ModState[k]
@@ -39,10 +39,10 @@ func (ks *KeySeq) IsPressed(key any) bool {
 	return false
 }
 
-func (ks *KeySeq) ArePressed(keys ...any) bool {
+func (ks *KeySeq) Pressed(keys ...any) bool {
 	pressed := []any{}
 	for _, key := range keys {
-		if ks.IsPressed(key) {
+		if ks.isPressed(key) {
 			pressed = append(pressed, key)
 		}
 	}
@@ -70,18 +70,14 @@ func (ks *KeySeq) Update(mk Mods_Key) {
 func (ks *KeySeq) Str() string {
 	var parts []string
 
-	// ugly code, but always sorted by Shift, Ctrl, Opt, Cmd
-	if pressed, exists := ks.seq.ModState[MOD_SHIFT]; exists && pressed {
-		parts = append(parts, modNames[MOD_SHIFT])
-	}
-	if pressed, exists := ks.seq.ModState[MOD_CTRL]; exists && pressed {
-		parts = append(parts, modNames[MOD_CTRL])
-	}
-	if pressed, exists := ks.seq.ModState[MOD_OPT]; exists && pressed {
-		parts = append(parts, modNames[MOD_OPT])
-	}
-	if pressed, exists := ks.seq.ModState[MOD_CMD]; exists && pressed {
-		parts = append(parts, modNames[MOD_CMD])
+	modOrder := []uint64{MOD_SHIFT, MOD_CTRL, MOD_OPT, MOD_CMD}
+
+	for _, mod := range modOrder {
+		if pressed, exists := ks.seq.ModState[mod]; exists && pressed {
+			if modName, ok := modNames[mod]; ok {
+				parts = append(parts, modName)
+			}
+		}
 	}
 
 	if ks.seq.Key != KEY_NONE {
